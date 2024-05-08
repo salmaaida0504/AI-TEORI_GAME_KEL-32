0bd0604115935aa5349d280fabb8bd3ae00fdecf
salmaaida0504 <salmasay23@gmail.com>
2024-05-08 09:27:16 +0700

    first commit pacman kel 32

---
+++ pacman.py
@@ -0,0 +1,206 @@
+import copy
+from board import boards
+import pygame
+import math
+import heapq
+
+laptop_width = 900
+laptop_height = 950
+
+pygame.init()
+
+WIDTH = laptop_width
+HEIGHT = laptop_height
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+timer = pygame.time.Clock()
+fps = 60
+font = pygame.font.Font('freesansbold.ttf', 20)
+level = copy.deepcopy(boards)
+color = 'blue'
+PI = math.pi
+player_images = []
+for i in range(1, 5):
+    player_images.append(pygame.transform.scale(pygame.image.load(f'assets/player_images/{i}.png'), (45, 45)))
+blinky_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/red.png'), (45, 45))
+pinky_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/pink.png'), (45, 45))
+inky_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/blue.png'), (45, 45))
+clyde_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/orange.png'), (45, 45))
+spooked_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/powerup.png'), (45, 45))
+dead_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/dead.png'), (45, 45))
+player_x = 450
+player_y = 663
+direction = 0
+blinky_x = 56
+blinky_y = 58
+blinky_direction = 0
+inky_x = 440
+inky_y = 388
+inky_direction = 2
+pinky_x = 440
+pinky_y = 438
+pinky_direction = 2
+clyde_x = 440
+clyde_y = 438
+clyde_direction = 2
+counter = 0
+flicker = False
+# R, L, U, D
+turns_allowed = [False, False, False, False]
+direction_command = 0
+player_speed = 2
+score = 0
+powerup = False
+power_counter = 0
+eaten_ghost = [False, False, False, False]
+targets = [(player_x, player_y), (player_x, player_y), (player_x, player_y), (player_x, player_y)]
+blinky_dead = False
+inky_dead = False
+clyde_dead = False
+pinky_dead = False
+blinky_box = False
+inky_box = False
+clyde_box = False
+pinky_box = False
+moving = False
+ghost_speeds = [2, 2, 2, 2]
+startup_counter = 0
+lives = 3
+game_over = False
+game_won = False
+
+def heuristic(pos1, pos2):
+    x1, y1 = pos1
+    x2, y2 = pos2
+    return abs(x1 - x2) + abs(y1 - y2)
+
+class Ghost:
+    def __init__(self, x_coord, y_coord, target, speed, img, direct, dead, box, id):
+        self.x_pos = x_coord
+        self.y_pos = y_coord
+        self.center_x = self.x_pos + 22
+        self.center_y = self.y_pos + 22
+        self.target = target
+        self.speed = speed
+        self.img = img
+        self.direction = direct
+        self.dead = dead
+        self.in_box = box
+        self.id = id
+        self.turns, self.in_box = self.check_collisions()
+        self.rect = self.draw()
+
+    def draw(self):
+        if (not powerup and not self.dead) or (eaten_ghost[self.id] and powerup and not self.dead):
+            screen.blit(self.img, (self.x_pos, self.y_pos))
+        elif powerup and not self.dead and not eaten_ghost[self.id]:
+            screen.blit(spooked_img, (self.x_pos, self.y_pos))
+        else:
+            screen.blit(dead_img, (self.x_pos, self.y_pos))
+        ghost_rect = pygame.rect.Rect((self.center_x - 18, self.center_y - 18), (36, 36))
+        return ghost_rect
+
+    def check_collisions(self):
+        # R, L, U, D
+        num1 = ((HEIGHT - 50) // 32)
+        num2 = (WIDTH // 30)
+        num3 = 15
+        self.turns = [False, False, False, False]
+        if 0 < self.center_x // 30 < 29:
+            if level[(self.center_y - num3) // num1][self.center_x // num2] == 9:
+                self.turns[2] = True
+            if level[self.center_y // num1][(self.center_x - num3) // num2] < 3 \
+                    or (level[self.center_y // num1][(self.center_x - num3) // num2] == 9 and (
+                    self.in_box or self.dead)):
+                self.turns[1] = True
+            if level[self.center_y // num1][(self.center_x + num3) // num2] < 3 \
+                    or (level[self.center_y // num1][(self.center_x + num3) // num2] == 9 and (
+                    self.in_box or self.dead)):
+                self.turns[0] = True
+            if level[(self.center_y + num3) // num1][self.center_x // num2] < 3 \
+                    or (level[(self.center_y + num3) // num1][self.center_x // num2] == 9 and (
+                    self.in_box or self.dead)):
+                self.turns[3] = True
+            if level[(self.center_y - num3) // num1][self.center_x // num2] < 3 \
+                    or (level[(self.center_y - num3) // num1][self.center_x // num2] == 9 and (
+                    self.in_box or self.dead)):
+                self.turns[2] = True
+
+            if self.direction == 2 or self.direction == 3:
+                if 12 <= self.center_x % num2 <= 18:
+                    if level[(self.center_y + num3) // num1][self.center_x // num2] < 3 \
+                            or (level[(self.center_y + num3) // num1][self.center_x // num2] == 9 and (
+                            self.in_box or self.dead)):
+                        self.turns[3] = True
+                    if level[(self.center_y - num3) // num1][self.center_x // num2] < 3 \
+                            or (level[(self.center_y - num3) // num1][self.center_x // num2] == 9 and (
+                            self.in_box or self.dead)):
+                        self.turns[2] = True
+                if 12 <= self.center_y % num1 <= 18:
+                    if level[self.center_y // num1][(self.center_x - num2) // num2] < 3 \
+                            or (level[self.center_y // num1][(self.center_x - num2) // num2] == 9 and (
+                            self.in_box or self.dead)):
+                        self.turns[1] = True
+                    if level[self.center_y // num1][(self.center_x + num2) // num2] < 3 \
+                            or (level[self.center_y // num1][(self.center_x + num2) // num2] == 9 and (
+                            self.in_box or self.dead)):
+                        self.turns[0] = True
+
+            if self.direction == 0 or self.direction == 1:
+                if 12 <= self.center_x % num2 <= 18:
+                    if level[(self.center_y + num3) // num1][self.center_x // num2] < 3 \
+                            or (level[(self.center_y + num3) // num1][self.center_x // num2] == 9 and (
+                            self.in_box or self.dead)):
+                        self.turns[3] = True
+                    if level[(self.center_y - num3) // num1][self.center_x // num2] < 3 \
+                            or (level[(self.center_y - num3) // num1][self.center_x // num2] == 9 and (
+                            self.in_box or self.dead)):
+                        self.turns[2] = True
+                if 12 <= self.center_y % num1 <= 18:
+                    if level[self.center_y // num1][(self.center_x - num3) // num2] < 3 \
+                            or (level[self.center_y // num1][(self.center_x - num3) // num2] == 9 and (
+                            self.in_box or self.dead)):
+                        self.turns[1] = True
+                    if level[self.center_y // num1][(self.center_x + num3) // num2] < 3 \
+                            or (level[self.center_y // num1][(self.center_x + num3) // num2] == 9 and (
+                            self.in_box or self.dead)):
+                        self.turns[0] = True
+        else:
+            self.turns[0] = True
+            self.turns[1] = True
+        if 350 < self.x_pos < 550 and 370 < self.y_pos < 480:
+            self.in_box = True
+        else:
+            self.in_box = False
+        return self.turns, self.in_box
+
+    def find_path(self, level, target):
+        open_list = []
+        closed_list = set()
+        start_node = (self.center_x // 30, self.center_y // 32)
+        target_node = (target[0] // 30, target[1] // 32)
+
+        heapq.heappush(open_list, (0, start_node))
+
+        while open_list:
+            current_cost, current_node = heapq.heappop(open_list)
+
+            if current_node == target_node:
+                return current_cost
+
+            if current_node in closed_list:
+                continue
+
+            closed_list.add(current_node)
+
+            x, y = current_node
+            neighbors = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+
+            for neighbor in neighbors:
+                nx, ny = neighbor
+                if 0 <= nx < len(level[0]) and 0 <= ny < len(level):
+                    if level[ny][nx] < 3 or level[ny][nx] == 9:
+                        neighbor_cost = current_cost + 1
+                        heapq.heappush(open_list, (neighbor_cost + heuristic(neighbor, target_node), neighbor))
+
+        return float('inf')
+
---
+++ pakai yang ini.py
@@ -0,0 +1,322 @@
+# Import library yang diperlukan
+import pygame
+import random
+import math
+from queue import PriorityQueue
+import os
+
+# Inisialisasi ukuran layar dan grid
+CELL_SIZE = 20
+GRID_WIDTH = 28
+GRID_HEIGHT = 31
+MARGIN = 1
+SCREEN_WIDTH = GRID_WIDTH * CELL_SIZE + (GRID_WIDTH + 1) * MARGIN
+SCREEN_HEIGHT = GRID_HEIGHT * CELL_SIZE + (GRID_HEIGHT + 1) * MARGIN
+
+# Inisialisasi arah pergerakan
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+# Inisialisasi warna
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+
+# Inisialisasi kecepatan gerakan hantu
+GHOST_SPEED = 0.05
+
+# Inisialisasi kelas node
+class Node:
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+        self.wall = False
+
+# Inisialisasi kelas Pacman
+class Pacman:
+    def __init__(self, row, col, image):
+        self.row = row
+        self.col = col
+        self.score = 0
+        self.image = pygame.transform.scale(pygame.image.load(image), (20, 20))  # Ubah ukuran gambar Pac-Man
+        self.rect = self.image.get_rect()
+
+    def move(self, direction, grid):
+        next_row = self.row + direction[1]
+        next_col = self.col + direction[0]
+
+        # Periksa apakah sel yang akan dituju bukan dinding
+        if 0 <= next_row < GRID_HEIGHT and 0 <= next_col < GRID_WIDTH and not grid[next_row][next_col].wall:
+            self.row = next_row
+            self.col = next_col
+
+# Inisialisasi kelas Ghost
+class Ghost:
+    def __init__(self, row, col, color, behavior):
+        self.row = row
+        self.col = col
+        self.color = color
+        self.behavior = behavior
+        self.path = []
+        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])  # Inisialisasi arah acak
+
+    def heuristic(self, a, b):
+        return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+    def find_path(self, grid, target):
+        open_list = PriorityQueue()
+        open_list.put((0, (self.row, self.col)))
+        came_from = {}
+        cost_so_far = {(self.row, self.col): 0}
+
+        while not open_list.empty():
+            current_cost, current_node = open_list.get()
+
+            if current_node == target:
+                self.path = self.reconstruct_path(came_from, current_node)
+                return
+
+            for direction in [UP, DOWN, LEFT, RIGHT]:
+                next_row = current_node[0] + direction[1]
+                next_col = current_node[1] + direction[0]
+
+                if not (0 <= next_row < GRID_HEIGHT and 0 <= next_col < GRID_WIDTH):
+                    continue
+
+                neighbor = (next_row, next_col)
+                new_cost = cost_so_far[current_node] + 1
+
+                if neighbor not in cost_so_far or new_cost < cost_so_far[neighbor]:
+                    cost_so_far[neighbor] = new_cost
+                    priority = new_cost + self.heuristic(target, neighbor)
+                    open_list.put((priority, neighbor))
+                    came_from[neighbor] = current_node
+
+    def reconstruct_path(self, came_from, current_node):
+        path = []
+        while current_node in came_from:
+            path.append(current_node)
+            current_node = came_from[current_node]
+        path.reverse()
+        return path
+
+    def move(self, grid):
+        if random.random() < GHOST_SPEED:
+            # Perbarui arah jika hantu sudah mencapai titik tujuan dalam jalur
+            if not self.path or (self.row, self.col) == self.path[-1]:
+                if self.behavior == "chase":
+                    self.chase(grid)
+                elif self.behavior == "scatter":
+                    self.scatter()
+                elif self.behavior == "frightened":
+                    self.frightened(grid)
+                elif self.behavior == "return_home":
+                    self.return_home()
+
+            next_row = self.row + self.direction[1]
+            next_col = self.col + self.direction[0]
+
+            # Periksa apakah sel yang akan dituju bukan dinding
+            if 0 <= next_row < GRID_HEIGHT and 0 <= next_col < GRID_WIDTH and not grid[next_row][next_col].wall:
+                self.row = next_row
+                self.col = next_col
+            else:
+                # Jika bertabrakan dengan dinding, cari arah lain yang kosong
+                possible_directions = [UP, DOWN, LEFT, RIGHT]
+                random.shuffle(possible_directions)
+                for direction in possible_directions:
+                    new_row = self.row + direction[1]
+                    new_col = self.col + direction[0]
+                    if 0 <= new_row < GRID_HEIGHT and 0 <= new_col < GRID_WIDTH and not grid[new_row][new_col].wall:
+                        self.row = new_row
+                        self.col = new_col
+                        self.direction = direction
+                        break
+
+    def chase(self, grid, pacman):
+        target = (pacman.row, pacman.col)
+        self.find_path(grid, target)
+
+    def scatter(self, grid):
+        # Define target positions for each ghost based on their behavior
+        target_positions = {
+            "blinky": (0, GRID_WIDTH - 1),  # Top right corner
+            "pinky": (0, 0),  # Top left corner
+            "inky": (GRID_HEIGHT - 1, GRID_WIDTH - 1),  # Bottom right corner
+            "clyde": (GRID_HEIGHT - 1, 0)  # Bottom left corner
+        }
+        target = target_positions[self.color]
+        self.find_path(grid, target)
+
+    def frightened(self, grid, pacman):
+        # Scatter behavior can be used when ghosts are frightened
+        self.scatter(grid)
+
+    def return_home(self, grid):
+        # Define target positions for each ghost to return to their home position
+        home_positions = {
+            "blinky": (GRID_HEIGHT // 2 - 3, GRID_WIDTH // 2),
+            "pinky": (GRID_HEIGHT // 2 - 3, GRID_WIDTH // 2 + 1),
+            "inky": (GRID_HEIGHT // 2 + 2, GRID_WIDTH // 2),
+            "clyde": (GRID_HEIGHT // 2 + 2, GRID_WIDTH // 2 + 1)
+        }
+        target = home_positions[self.color]
+        self.find_path(grid, target)
+
+
+# Fungsi untuk menggambar grid
+def draw_grid(grid, screen):
+    for row in range(GRID_HEIGHT):
+        for col in range(GRID_WIDTH):
+            color = BLACK
+            if grid[row][col].wall:
+                color = BLUE
+            pygame.draw.rect(screen, color, [(MARGIN + CELL_SIZE) * col + MARGIN,
+                                              (MARGIN + CELL_SIZE) * row + MARGIN,
+                                              CELL_SIZE,
+                                              CELL_SIZE])
+
+# Fungsi untuk membuat labirin
+def create_maze():
+    maze = [[Node(row, col) for col in range(GRID_WIDTH)] for row in range(GRID_HEIGHT)]
+
+    # Inisialisasi semua sel sebagai dinding
+    for row in range(GRID_HEIGHT):
+        for col in range(GRID_WIDTH):
+            maze[row][col].wall = True
+
+    # Mulai dari sel tengah dan lakukan rekursi untuk membuka jalan
+    start_row, start_col = GRID_HEIGHT // 2, GRID_WIDTH // 2
+    maze[start_row][start_col].wall = False
+    stack = [(start_row, start_col)]
+
+    while stack:
+        current_row, current_col = stack[-1]
+        neighbors = [(current_row + 2, current_col), (current_row - 2, current_col),
+                     (current_row, current_col + 2), (current_row, current_col - 2)]
+        unvisited_neighbors = [(row, col) for row, col in neighbors if 0 <= row < GRID_HEIGHT and 0 <= col < GRID_WIDTH and maze[row][col].wall]
+        if unvisited_neighbors:
+            next_row, next_col = random.choice(unvisited_neighbors)
+            maze[next_row][next_col].wall = False
+            maze[(current_row + next_row) // 2][(current_col + next_col) // 2].wall = False
+            stack.append((next_row, next_col))
+        else:
+            stack.pop()
+
+    return maze
+
+# Fungsi untuk menentukan posisi awal hantu dengan jarak yang cukup jauh dari Pac-Man
+def generate_ghost_positions(pacman_row, pacman_col):
+    max_distance = 10  # Jarak minimal antara Pac-Man dan hantu
+    ghost_positions = []
+
+    while len(ghost_positions) < 4:  # Jumlah hantu
+        ghost_row = random.randint(0, GRID_HEIGHT - 1)
+        ghost_col = random.randint(0, GRID_WIDTH - 1)
+        # Hitung jarak Euclidean antara Pac-Man dan hantu
+        distance = math.sqrt((ghost_row - pacman_row) ** 2 + (ghost_col - pacman_col) ** 2)
+        if distance >= max_distance:
+            ghost_positions.append((ghost_row, ghost_col))
+
+    return ghost_positions
+
+# Fungsi utama
+def main():
+    pygame.init()
+
+    # Inisialisasi layar
+    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+    pygame.display.set_caption("Pac-Man")
+
+    # Inisialisasi labirin
+    grid = create_maze()
+
+    # Inisialisasi karakter Pac-Man
+    pacman_image = os.path.join("assets/player_images", "1.png")
+    pacman = Pacman(13, 15, pacman_image)
+
+    # Inisialisasi hantu
+    ghost_images = [os.path.join("assets/ghost_images", "red.png"),
+                    os.path.join("assets/ghost_images", "pink.png"),
+                    os.path.join("assets/ghost_images", "blue.png"),
+                    os.path.join("assets/ghost_images", "orange.png")]
+    ghost_behaviors = ["chase", "scatter", "frightened", "return_home"]  # Define behavior for each ghost
+    ghost_positions = generate_ghost_positions(pacman.row, pacman.col)
+    ghosts = [Ghost(row, col, ghost_images[i], ghost_behaviors[i]) for i, (row, col) in enumerate(ghost_positions)]
+
+    
+    # Inisialisasi umpan
+    food_positions = [(row, col) for row in range(GRID_HEIGHT) for col in range(GRID_WIDTH)
+                    if not grid[row][col].wall and (row, col) != (pacman.row, pacman.col)]
+    for ghost in ghosts:
+        food_positions = [(row, col) for (row, col) in food_positions if (row, col) != (ghost.row, ghost.col)]
+    food_positions = random.sample(food_positions, len(food_positions) // 2)  # Ambil setengah dari posisi makanan
+    foods = food_positions
+
+    # Main loop
+    running = True
+    pacman_automatic_movement_timer = 300  # Waktu dalam frame (300 frames = 5 detik)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    pacman.move(UP, grid)
+                elif event.key == pygame.K_DOWN:
+                    pacman.move(DOWN, grid)
+                elif event.key == pygame.K_LEFT:
+                    pacman.move(LEFT, grid)
+                elif event.key == pygame.K_RIGHT:
+                    pacman.move(RIGHT, grid)
+
+        # Gerakkan hantu
+        for ghost in ghosts:
+            ghost.move(grid)
+            ghost.chase(grid, pacman)  # Berikan objek pacman sebagai argumen
+
+        # Hapus makanan yang dimakan oleh Pac-Man
+        if (pacman.row, pacman.col) in food_positions:
+            food_positions.remove((pacman.row, pacman.col))
+            pacman.score += 10
+
+        # Cek tabrakan antara Pac-Man dan hantu
+        for ghost in ghosts:
+            if (pacman.row, pacman.col) == (ghost.row, ghost.col):
+                print("Game Over! Your score:", pacman.score)
+                pygame.quit()
+                return
+
+        # Clear the screen and draw the grid
+        screen.fill(BLACK)  # Ubah latar belakang layar menjadi hitam
+        draw_grid(grid, screen)
+
+        # Gambar makanan
+        for food in foods:
+            pygame.draw.circle(screen, WHITE, ((MARGIN + CELL_SIZE) * food[1] + MARGIN + CELL_SIZE // 2,
+                                                (MARGIN + CELL_SIZE) * food[0] + MARGIN + CELL_SIZE // 2), CELL_SIZE // 6)
+
+        # Gambar Pac-Man
+        screen.blit(pacman.image, ((MARGIN + CELL_SIZE) * pacman.col + MARGIN, (MARGIN + CELL_SIZE) * pacman.row + MARGIN))
+
+        # Gambar hantu
+        for ghost in ghosts:
+            ghost_image = pygame.transform.scale(pygame.image.load(ghost.color), (20, 20))  # Ubah ukuran gambar hantu
+            screen.blit(ghost_image, ((MARGIN + CELL_SIZE) * ghost.col + MARGIN, (MARGIN + CELL_SIZE) * ghost.row + MARGIN))
+
+        # Update the display
+        pygame.display.flip()
+
+        # Pergerakan otomatis Pac-Man setelah 5 detik
+        if pacman_automatic_movement_timer > 0:
+            pacman_automatic_movement_timer -= 1
+        else:
+            # Implementasi pergerakan otomatis di sini
+            pass
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
